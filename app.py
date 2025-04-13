3c25440a8d8b3466c290a07b9abad4e96895256c
Sameer arora <73692669+sameeross@users.noreply.github.com>
2025-04-13 19:47:00 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,23 @@ import time
 import requests
 import json
 from flask import Flask, request
+import google.generativeai as genai
 
+genai.configure(api_key="AIzaSyCXRRkJ6Uez-Lm5c0cxqP9YNRhwsE_hYGE")
+
+model = genai.get_tuned_model(f'tunedModels/generate-num-7807')
+model2 = genai.GenerativeModel(model_name=f'tunedModels/generate-num-7807')
+result = model2.generate_content('how can i book tickets')
 
 webhook_url_post = "https://sameerarora.app.n8n.cloud/webhook-test/989cb120-9ab9-425b-8a48-99c351bba433"
 
 
-# Streamed response emulator
-def response_generator():
+
+def response_generator(prompt):
+    result = model2.generate_content(prompt)
     response = random.choice(
         [
-            "Hello there! How can I assist you today?" ,
-            "Hi, human! Is there anything I can help you with?"  ,
-            "Do you need help?" ,
+            result.text
         ]
     )
     for word in response.split():
@@ -25,11 +30,10 @@ def response_generator():
 
 st.title("Ai chatbot")
 
-# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
+# Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
@@ -41,14 +45,14 @@ if prompt := st.chat_input("What is up?"):
     data_for_post = {"role": "user", "content": prompt}
     r = requests.post(webhook_url_post,json.dumps(data_for_post),headers = {'Content-Type':'application/json'})
 
-    # Display user message in chat message container
+    # Display message 
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response in chat message container
+    # Display response 
     with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
-    # Add assistant response to chat history
+        
+        response = st.write_stream(response_generator(prompt))
+   
     st.session_state.messages.append({"role": "assistant", "content": response})
-
 
